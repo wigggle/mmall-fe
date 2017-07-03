7944e9749e2209aa4ffa61a16cf44eba1cb5696e
qishuang2016@163.com <3649093shuang>
2017-07-03 15:04:17 +0800

    initial

---
+++ src/page/common/index.js
@@ -0,0 +1,5 @@
+/**
+ * Created by mshuang on 2017/7/2.
+ */
+'use strict';
+console.log('I am global');
---
+++ src/page/login/index.js
@@ -0,0 +1,6 @@
+/**
+ * Created by mshuang on 2017/7/2.
+ */
+'use strict';
+require('../module.js');
+console.log('hello login')
